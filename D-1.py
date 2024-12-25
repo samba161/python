90c388b70f62446182213fc4c54ff905d89604d8
samba <gsambasiva161@gmail.com>
2024-12-25 10:28:45 +0530

    python:

---
+++ D-1.py
@@ -0,0 +1,26 @@
+# To print the sum of 2 numbers
+a=6
+b=6
+c=a+b
+print("The sum of two numbers:",(c))
+
+
+#A Name in python is called identifier
+#pyhton allowed characters are alphabet,digits and (_) underscore
+# ex cash =10
+#identifier should not start with digit
+#python is case sensitive
+#we cannot use reserved words as identifiers ex def=10
+#dollor $ symbol is not allowed in python
+# if identifier is start with _symbol then it indicates that is is private
+#if identifier starts with __(two under score) indicating that strongly private
+#__add__ is special name/Magical methods
+
+print(type(a))
+print(id(a))
+
+print(bin(15))
+print(bin(0o11))
+print(bin(0X10))
+a=True
+print(type(a))
---
+++ Day-2.py
@@ -0,0 +1,6 @@
+speech = "She said: \"Hi\""
+print(speech)
+Days = 365
+print(f"There are {Days}  days in a year")
+type(Days)
+print(type(Days))
